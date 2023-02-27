0439b4a10586f90af23d3d0e0a5f14d2bfd3e8ea
levi <190031@buas.nl>
2023-02-27 21:48:41 +0100

    update to image.h
    cleaned up image.h

--- ASCII_Converter/ASCII_Converter/Include/Image.h
+++ ASCII_Converter/ASCII_Converter/Include/Image.h
@@ -2,6 +2,8 @@
 #include <string>
 #include <vector>
 #include "dynamic_bitset.hpp"
+#include <type_traits>
+#include <functional>
 
 namespace ASCII
 {
@@ -27,6 +29,14 @@ public:
 		JPG
 	};
 
+	struct GifColor
+	{
+		uint8_t R = 0;
+		uint8_t G = 0;
+		uint8_t B = 0;
+		int EntryIndex;
+	};
+
 	Image(const std::string& inPath, EChannels inChannel = EChannels::UseImageData);
 	Image(int32_t inWidth, int32_t inHeight, EChannels inChannel = EChannels::RGB, int32_t inFrames = 1);
 	~Image();
@@ -42,30 +52,45 @@ public:
 	void CopyRect(const Image& inImage, int32_t inSrcX, int32_t inSrcY, int32_t inDestX, int32_t inDestY, int32_t x, int32_t y);
 	void ExportImage(const std::string& inExportPath, ExportAs inExportAs = ExportAs::JPG, int Quality = 100) const;
 	int32_t GetFrames() const { return mFrames; }
+	void BuildColorTable();
+
 private:
 	void LoadFromDisk(const std::string& inPath, EChannels inChannel);
+
+
 	void SaveAnimatedGifToFile(const std::string& inExportPath) const;
 
 	void ProcessFrame(size_t frame, std::ofstream& outfile, const sul::dynamic_bitset<>& inBitfield) const;
 
 
-	void WriteGraphicControlExtention(size_t& frame, std::ofstream& outfile) const;
+	void WriteGraphicControlExtention(size_t frame, std::ofstream& outfile) const;
 	void WriteApplicationExentionBlock(std::ofstream& outfile) const;
-	void WriteImageDescriptor(std::ofstream& outfile) const;
+	void WriteImageDescriptor(size_t frame, std::ofstream& outfile) const;
 	void Compress(std::vector<int>& indexStream, size_t frame, sul::dynamic_bitset<>& inBitField) const;
 
-	void SaveBitField(const sul::dynamic_bitset<>& bitField, std::ofstream& outfile) const;
-	void BuildBitField(std::vector<std::vector<int>> codeStreams, sul::dynamic_bitset<>& bitField) const;
+	void SaveBitField(size_t frame, const sul::dynamic_bitset<>& bitField, std::ofstream& outfile) const;
 
-	std::vector<int> BuildIndexStream(const size_t& frame, GifHeader& header) const;
-
+	std::vector<int> BuildIndexStream(size_t frame, GifHeader& header) const;
+	using ColorTable = std::unordered_map<size_t, GifColor>;
+	using ColorTableList = std::vector<ColorTable>;
 	int32_t		mWidth;
 	int32_t		mHeight;
 	int32_t		mChannels;
 	int32_t		mFrames = 1;
 	uint8_t*	mPixels = nullptr;
 	int32_t*	mDelays = nullptr;
+	ColorTableList	mColorTableList;
 };
-
 }
 
+template<>
+struct std::hash<ASCII::Image::GifColor>
+{
+	std::size_t operator()(ASCII::Image::GifColor const& gifColor) const noexcept
+	{
+		std::size_t h1 = ((size_t)gifColor.R << 16);
+		std::size_t h2 = ((size_t)gifColor.G << 8);
+		std::size_t h3 = gifColor.B;
+		return h1 | h2 | h3;
+	}
+};
